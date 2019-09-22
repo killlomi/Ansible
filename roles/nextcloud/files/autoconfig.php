4c92d16579a8930d1debe2427e1515c6a65b0a70
Artem <killlominot@yandex.ru>
2019-09-22 21:23:06 +0300

    Попытки с nextcloud

---
+++ roles/nextcloud/files/autoconfig.php
@@ -0,0 +1,13 @@
+<? PHP
+
+ $AUTOCONFIG = array(
+  "dbtype"        => "mysql",
+  "dbname"        => "nextcloud",
+  "dbuser"        => "nextcloud",
+  "dbpass"        => "12345",
+  "dbhost"        => "db",
+  "dbtableprefix" => "",
+  "adminlogin"    => "admin",
+  "adminpass"     => "12345",
+  "directory"     => "/var/www/html/data",
+);
